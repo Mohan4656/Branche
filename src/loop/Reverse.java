d172b2bd9bc02293046aaa96c0be80f8f057335f
Mohan4656 <mohanprasath4656@gmail.com>
2024-05-23 12:32:04 +0530

    New Git works

---
+++ src/loop/Reverse.java
@@ -0,0 +1,22 @@
+package loop;
+
+public class Reverse {
+	
+public static void main(String[] args) {
+		
+		int no=123456,rev=0;
+		 while(no!=0) {
+			 
+		//datatype variable = value (get last digit form number)
+			int reminder = (no%10);    //123456%10   = 12345.6
+			rev=(rev*10 + reminder);   //0*10+12345.6 = 12345.6
+		
+//	
+		//in this method remove last digit from number
+			
+			no=(no/10);        //123456/10   = 12345.6
+			
+			System.out.print(reminder);
+
+}
+}}
